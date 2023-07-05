a43417f45df5d8ab6d706bb16246748f6b3c9479
Ashi1810 <137042215+Ashi1810@users.noreply.github.com>
2023-07-05 23:21:57 +0530

    Module1.sol

--- Module1.sol
+++ Module1.sol
@@ -1,32 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import "hardhat/console.sol";
 
-contract errorHandler{
-    uint public age;
+contract errorHandler {
 
-    function setAge(uint _age) public {
-        age=_age;
-    }
+uint public age;
 
-    function testAssert() public view{
-        assert(age>=18);
-        console.log("You Are Adult");
+function setAge(uint _age) public {
+     age = _age;
+}
 
+function testAssert() public view {
+    assert(age>=18);
+    console.log("You are not a minor");
+}
+function testRequire() public view {
+    require(age>=18, "Minors are not allowed here");
+    console.log("Welcome, You are not a minor");
+}
+function testRevert() public view {
+    if(age>=18) { 
+    console.log("Welcome, You are not a minor");
     }
-    function testRequire() public view{
-        require(age>=18, "Minors are not allowed ");
-        console.log("You Are Adult");
-        
+    else{
+        revert("Minors are not allowed here");
     }
-    function testRevert() public view{
-        if(age>=18){
-            console.log("You are Adult");
-        }
-        else{
-            revert("Minors Not Allowed");
-        }
-        
-    }
+ }
 }
